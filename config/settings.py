fa32517d2339bdba809288fe56fc17df657de615
valdenicio31 <valdenicio31@gmail.com>
2026-08-02 14:15:27 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
