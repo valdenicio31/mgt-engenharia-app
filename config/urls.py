301753204e82cd5096aa2fa847a1b7f8b5501b87
valdenicio31 <valdenicio31@gmail.com>
2026-08-03 15:06:14 -0300

    Corrige exibicao de fotos no Render

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from core.forms import EmailCPFAuthenticationForm
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -14,4 +15,8 @@ urlpatterns = [
     path("redefinir-senha/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path("redefinir-senha/concluido/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
     path("", include("core.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
